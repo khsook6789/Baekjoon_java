6f711198b695113af5c0f6727fd2fa3936692636
khsook6789 <141126380+khsook6789@users.noreply.github.com>
2026-01-06 18:10:45 +0900

    [Bronze III] Title: 학점계산, Time: 168 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        HashMap<String,Object> map = new HashMap<String,Object>();
+        map.put("A+",4.3);
+        map.put("A0",4.0);
+        map.put("A-",3.7);
+        map.put("B+",3.3);
+        map.put("B0",3.0);
+        map.put("B-",2.7);
+        map.put("C+",2.3);
+        map.put("C0",2.0);
+        map.put("C-",1.7);
+        map.put("D+",1.3);
+        map.put("D0",1.0);
+        map.put("D-",0.7);
+        map.put("F",0.0);
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        System.out.println(map.get(str));
+
+    }
+}
