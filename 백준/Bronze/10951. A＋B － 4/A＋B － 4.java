e531c6b9c61aaf24068865820e24814aff633552
khsook6789 <141126380+khsook6789@users.noreply.github.com>
2026-01-06 18:18:21 +0900

    [Bronze V] Title: A+B - 4, Time: 188 ms, Memory: 17796 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+
+            System.out.println(a+b);
+        }
+    }
+}
