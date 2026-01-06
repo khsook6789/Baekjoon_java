0dda3c26844dff5f97808a59ad07481b03f6306b
khsook6789 <141126380+khsook6789@users.noreply.github.com>
2026-01-06 18:05:55 +0900

    [Bronze V] Title: 검증수, Time: 176 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+
+        int f = (a*a+b*b+c*c+d*d+e*e)%10;
+        
+        System.out.println(f);
+    }
+}
