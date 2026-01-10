0f24d4eaf837e8ecbc7b29a4b2d1a2b919f52063
khsook6789 <141126380+khsook6789@users.noreply.github.com>
2026-01-10 17:22:29 +0900

    [Silver IV] Title: 설탕 배달, Time: 100 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -5,8 +5,9 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
 
         if(n==4||n==7){
             System.out.println(-1);
