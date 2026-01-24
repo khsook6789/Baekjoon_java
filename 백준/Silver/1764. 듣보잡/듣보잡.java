d2818284bcc76b6a0e73484c3f36e18dcfbce31a
khsook6789 <141126380+khsook6789@users.noreply.github.com>
2026-01-24 16:45:56 +0900

    [Silver IV] Title: 듣보잡, Time: 300 ms, Memory: 29100 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -10,19 +10,19 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        HashMap<String,Integer> map = new HashMap<>(n);
+        HashMap<String,Integer> map = new HashMap<>();
         for(int i=0;i<n;i++) {
             map.put(br.readLine(),1);
         }
         
         List<String> list = new ArrayList<>();
-        
+
         for(int i=0;i<m;i++) {
             String name = br.readLine();
             map.put(name,map.getOrDefault(name,0)+1);
             if(map.get(name)==2){list.add(name);}
         }
-        
+
         Collections.sort(list);
         for(String s:list){
             sb.append(s).append("\n");
