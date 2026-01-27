6f937350237f587859625187b6e63aa8875b98fe
khsook6789 <141126380+khsook6789@users.noreply.github.com>
2026-01-27 19:05:39 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static boolean[] visited;
+    static int[][] graph;
+    static int count = 0;
+
+    public static void dfs(int n){
+        visited[n] = true;
+        count++;
+
+        for(int i=1;i<graph.length;i++){
+            if(graph[n][i]==1&& !visited[i]){
+                dfs(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine()); //컴퓨터 수
+        int m = Integer.parseInt(br.readLine()); // 연결 쌍 수
+
+        visited = new boolean[n + 1];
+        graph = new int[n + 1][n + 1];
+
+        for(int i=0;i<m;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a][b] = 1;
+            graph[b][a] = 1;
+        }
+
+        dfs(1);
+        System.out.println(count - 1);
+    }
+}
