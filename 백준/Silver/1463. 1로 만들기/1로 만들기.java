514bc5a98464c6cf24dfa1ea65115ca432198265
khsook6789 <141126380+khsook6789@users.noreply.github.com>
2026-01-27 16:19:13 +0900

    [Silver III] Title: 1로 만들기, Time: 108 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -2,29 +2,18 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static Integer[] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        dp = new Integer[n + 1];
-        dp[0] = dp[1] = 0;
-        System.out.println(recur(n));
+        System.out.println(recur(n,0));
     }
 
-    static int recur(int n){
-
-        if(dp[n]==null){
-            if(n%6==0){
-                dp[n] = Math.min(recur(n/3),Math.min(recur(n/2),recur(n-1)))+1;
-            }else if(n%3==0){
-                dp[n] = Math.min(recur(n/3),recur(n-1))+1;
-            }else if(n%2==0){
-                dp[n] = Math.min(recur(n/2),recur(n-1))+1;
-            }else{
-                dp[n] = recur(n-1)+1;
-            }
+    static int recur(int n, int count){
+        if(n<2){
+            return count;
         }
-        return dp[n];
+
+        return Math.min(recur(n/2,count+n%2+1),recur(n/3,count+n%3+1));
     }
 }
