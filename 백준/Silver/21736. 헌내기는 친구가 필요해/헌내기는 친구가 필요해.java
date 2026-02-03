78634661a268769908c10679afeea80b49fd9759
khsook6789 <141126380+khsook6789@users.noreply.github.com>
2026-02-03 17:39:16 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 268 ms, Memory: 45408 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
@@ -2,13 +2,14 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[][] map;
+    static char[][] map;
     static boolean[][] visited;
     static int[] dx = {0, 0, -1, 1};
     static int[] dy = {-1, 1, 0, 0};
     static int count = 0;
+    static int n, m;
 
-    public static void dfs(int x, int y, int n, int m){
+    public static void dfs(int x, int y){
         visited[x][y] = true;
 
         for(int i=0;i<4;i++){
@@ -16,12 +17,17 @@ public class Main {
             int ny = y + dy[i];
 
             if(nx>=0 && ny>=0 && nx<n && ny<m){
-                if(!visited[nx][ny] && map[nx][ny] != 0){
-                    if(map[nx][ny]==2){
-                        count++;
-                        dfs(nx, ny, n, m);
-                    }else if(map[nx][ny]==1){
-                        dfs(nx, ny, n, m);
+                if(!visited[nx][ny]){
+                    switch(map[nx][ny]){
+                        case 'O':
+                            dfs(nx,ny);
+                            break;
+                        case 'X':
+                            break;
+                        case 'P':
+                            count++;
+                            dfs(nx,ny);
+                            break;
                     }
                 }
             }
@@ -32,32 +38,26 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st =new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        map = new int[n][m];
+        map = new char[n][m];
         visited = new boolean[n][m];
         int startX = 0;
         int startY = 0;
 
         for(int i=0;i<n;i++){
-            String[] str = br.readLine().split("");
+            String s = br.readLine();
             for(int j=0;j<m;j++){
-                if(str[j].equals("O")){
-                    map[i][j] = 1; // 빈 공간
-                }else if(str[j].equals("X")){
-                    map[i][j] = 0; // 벽
-                }else if(str[j].equals("P")){
-                    map[i][j] = 2; // 사람
-                }else if(str[j].equals("I")){
+                map[i][j] = s.charAt(j);
+                if(map[i][j]=='I'){
                     startX = i;
                     startY = j;
-                    map[i][j] = 1;
                 }
             }
         }
 
-        dfs(startX, startY, n, m);
+        dfs(startX, startY);
 
         if(count == 0){
             System.out.println("TT");
