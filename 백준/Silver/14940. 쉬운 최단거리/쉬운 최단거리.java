68aa0dba1a18b20d82cb1d81c12fde0617be9ac7
khsook6789 <141126380+khsook6789@users.noreply.github.com>
2026-02-07 17:20:22 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 552 ms, Memory: 49808 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -3,29 +3,29 @@ import java.util.*;
 
 public class Main {
     static int[][] graph;
-    static boolean[][] visited;
+    static int[][] dist;
     static int n,m;
     static int[] dx = {-1,1,0,0};
     static int[] dy = {0,0,-1,1};
-    static int[][] result;
 
     static void bfs(int x, int y){
         Queue<int[]> q = new LinkedList<>();
-        visited[x][y] = true;
         q.offer(new int[]{x,y});
+        dist[x][y] = 0;
 
         while(!q.isEmpty()){
             int[] cur = q.poll();
+            int curX = cur[0];
+            int curY = cur[1];
 
             for(int i=0;i<4;i++){
-                int nx = cur[0] + dx[i];
-                int ny = cur[1] + dy[i];
+                int nx = curX + dx[i];
+                int ny = curY + dy[i];
 
                 if(nx>=0 && ny>=0 && nx<n && ny<m){
-                    if(!visited[nx][ny] && graph[nx][ny]==1){
-                        visited[nx][ny] = true;
-                        result[nx][ny] = result[cur[0]][cur[1]] + 1;
-                        q.offer(new int[]{nx,ny});
+                    if(graph[nx][ny] != 0 && dist[nx][ny] == -1){
+                        dist[nx][ny] = dist[curX][curY] + 1;
+                        q.offer(new int[]{nx, ny});
                     }
                 }
             }
@@ -39,8 +39,7 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         graph = new int[n][m];
-        visited = new boolean[n][m];
-        result = new int[n][m];
+        dist = new int[n][m];
 
         int startX = 0;
         int startY = 0;
@@ -49,24 +48,27 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             for(int j=0;j<m;j++){
                 graph[i][j] = Integer.parseInt(st.nextToken());
+                dist[i][j] = -1;
                 if(graph[i][j] == 2){
                     startX = i;
                     startY = j;
-                }else if(graph[i][j]==0){
-                    visited[i][j] = true;
                 }
             }
         }
         bfs(startX, startY);
 
+        StringBuilder sb = new StringBuilder();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(!visited[i][j]){
-                    result[i][j] = -1;
+                if(graph[i][j]==0){
+                    sb.append(0).append(" ");
+                }else{
+                    sb.append(dist[i][j]).append(" ");
                 }
-                System.out.print(result[i][j]+" ");
             }
-            System.out.println();
+            sb.append("\n");
         }
+
+        System.out.print(sb.toString());
     }
 }
