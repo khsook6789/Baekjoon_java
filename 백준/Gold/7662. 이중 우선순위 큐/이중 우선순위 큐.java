1ce5501ad4ad1629621bb2c4c4e72bb140f9a67d
khsook6789 <141126380+khsook6789@users.noreply.github.com>
2026-02-12 16:34:33 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 3044 ms, Memory: 433840 KB -BaekjoonHub

---
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int t = 0; t <T; t++){
+            int k = Integer.parseInt(br.readLine());
+            TreeMap<Integer, Integer> map = new TreeMap<>();
+
+            for(int i=0;i<k;i++){
+                String[] input = br.readLine().split(" ");
+
+                String cmd = input[0];
+                int n = Integer.parseInt(input[1]);
+
+                if(cmd.equals("I")){
+                    map.put(n, map.getOrDefault(n, 0) + 1);
+                }else if(cmd.equals("D")){
+                    if(map.isEmpty()){
+                        continue;
+                    }
+                    if(n==1){
+                        map.put(map.lastKey(),map.getOrDefault(map.lastKey(),0) - 1);
+                        if(map.get(map.lastKey())<1){
+                            map.remove(map.lastKey());
+                        }
+                    }else if(n==-1){
+                        map.put(map.firstKey(),map.getOrDefault(map.firstKey(),0) -1);
+                        if(map.get(map.firstKey())<1){
+                            map.remove(map.firstKey());
+                        }
+                    }
+                }
+            }
+
+            if(map.isEmpty()){
+                System.out.println("EMPTY");
+            }else{
+                System.out.println(map.lastKey()+" "+map.firstKey());
+            }
+        }
+    }
+}
